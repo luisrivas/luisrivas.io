56c7dc58c2d784febe1221ff2ae887c0b5054efc
Luis Rivas <luis.rivas024@gmail.com>
2022-08-27 10:32:58 -0500

    add: social media icons
    Added the Github, Linkedin, and Twitter external links.

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,6 @@
+import { Link } from "@remix-run/react";
 import styles from "~/styles/landing.css";
+import { FaTwitter, FaLinkedin, FaGithub } from 'react-icons/fa';
 
 export function links() {
   return [{ rel: "stylesheet", href: styles }];
@@ -8,10 +10,10 @@ export default function Index() {
   return (
     <div className="flex flex-col h-screen justify-between">
       <header className="container flex mx-auto py-12 max-w-7xl px-6 desktop:px-0">
-        <a href="/" className="text-black text-lg">
+        <Link to="/" className="text-black text-lg">
           <h1 className="font-bold leading-tight">Luis Rivas</h1>
           <h2 className="leading-tight">Software Engineer</h2>
-        </a>
+        </Link>
       </header>
       <main className="container mx-auto flex items-center justify-between max-w-7xl px-6 desktop:px-0">
         <div className="max-w-5xl">
@@ -22,9 +24,31 @@ export default function Index() {
             I am passionate about improving the performance of products through engineering, and I am constantly looking to build new things.
           </p>
         </div>
+        <div className="hidden desktop:flex flex-col gap-10 mt-36">
+          <a href="https://twitter.com/luis_to_string">
+            <FaTwitter title="My twitter" className="fill-black w-6 h-6" />
+          </a>
+          <a href="https://www.linkedin.com/">
+            <FaLinkedin title="My linkedin" className="fill-black w-6 h-6" />
+          </a>
+          <a href="https://github.com/luisrivas">
+            <FaGithub title="My github" className="fill-black w-6 h-6" />
+          </a>
+        </div>
       </main>
       <footer className="container mx-auto max-w-7xl flex justify-between items-center px-6 desktop:px-0 py-4 border-t">
         <span className="text-gray">Working from home with a dog on my lap.</span>
+        <div className="flex gap-8">
+          <a href="https://twitter.com/luis_to_string">
+            <FaTwitter title="My twitter" className="fill-black w-6 h-6" />
+          </a>
+          <a href="https://www.linkedin.com/">
+            <FaLinkedin title="My linkedin" className="fill-black w-6 h-6" />
+          </a>
+          <a href="https://github.com/luisrivas">
+            <FaGithub title="My github" className="fill-black w-6 h-6" />
+          </a>
+        </div>
       </footer>
     </div>
   )
