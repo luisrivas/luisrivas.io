83cbd4981d0c441cb02a46dc5c7f5d822355bc70
Luis Rivas <luis.rivas024@gmail.com>
2024-03-20 20:57:25 -0500

    feat(home): Add initial home page content and layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,111 +1,60 @@
 import Image from "next/image";
+import { FaGithub, FaLinkedin, FaXTwitter } from "react-icons/fa6";
+import { socialMedia } from "../lib/constants";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+    <main className="container mx-auto flex items-center justify-between max-w-5xl px-6 desktop:px-0 gap-4">
+      <div className="max-w-5xl mb-10 text-balance">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          height={208}
+          width={208}
+          className="mb-4 rounded-full max-sm:mx-auto"
+          src="https://avatars.githubusercontent.com/u/2474521?v=4"
+          alt="me like a memoji"
         />
+        <h3 className="text-5xl leading-tight font-black text-black">
+          Hey there! I&apos;m Luis Rivas.
+        </h3>
+        <p className="text-2xl leading-normal font-base text-gray mt-4">
+          I&apos;m a software engineer who&apos;s all about web development. I
+          love diving into planning, coding, and building new things from
+          scratch. Seeing an idea come to life and knowing that I played a part
+          in making it happen is so satisfying.
+        </p>
+        <p className="text-2xl leading-normal font-base text-gray mt-4">
+          I&apos;m always looking for ways to tweak and improve the things I
+          work on, kind of like how a chef keeps perfecting their signature
+          dish. I enjoy the problem-solving aspect of figuring out how to
+          enhance accessibility, speed up performance, and deliver more value to
+          the users in each new version.
+        </p>
+        <p className="text-2xl leading-normal font-base text-gray mt-4">
+          But you know what really gets me excited? Sharing what I know with
+          others. There&apos;s something special about collaborating with a
+          team, bouncing ideas off each other, and learning from one another. I
+          believe that when we share our knowledge and experiences, we can
+          achieve so much more together.
+        </p>
+        <p className="text-2xl leading-normal font-base text-gray mt-4">
+          So yeah, that&apos;s me in a nutshell - a passionate web developer
+          who&apos;s always eager to build, improve, and share. It&apos;s not
+          just a job for me; it&apos;s a way of life!
+        </p>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
+      <div className="hidden desktop:flex flex-col gap-10 mt-36">
+        <a href={socialMedia.twitter} target="_blank" rel="noopener noreferrer">
+          <FaXTwitter title="My twitter" className="fill-black w-6 h-6" />
         </a>
-
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          href={socialMedia.linkedin}
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
+          <FaLinkedin title="My linkedin" className="fill-black w-6 h-6" />
         </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+        <a href={socialMedia.github} target="_blank" rel="noopener noreferrer">
+          <FaGithub title="My github" className="fill-black w-6 h-6" />
         </a>
       </div>
     </main>
---
+++ lib/constants.ts
@@ -0,0 +1,5 @@
+export const socialMedia = {
+  twitter: 'https://twitter.com/luisrivas_io',
+  linkedin: 'https://www.linkedin.com/in/luis-rivas/',
+  github: 'https://github.com/luisrivas',
+} as const
